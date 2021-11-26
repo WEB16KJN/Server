abc074f2920b26857897de35ba29535f9d59859d
silver <silver36c@gmail.com>
2021-11-26 16:25:14 +0900

    style: change code format

--- functions/db/like.js
+++ functions/db/like.js
@@ -1,51 +1,50 @@
 const _ = require('lodash');
 const convertSnakeToCamel = require('./../lib/convertSnakeToCamel');
 
-
 const getLikeByPaperId = async (client, paperId) => {
-    const { rows } = await client.query(
-      `
+  const { rows } = await client.query(
+    `
       SELECT * FROM "like" l
       WHERE paper_id = $1
       `,
-      [paperId],
-    );
-    return convertSnakeToCamel.keysToCamel(rows);
-  };
+    [paperId],
+  );
+  return convertSnakeToCamel.keysToCamel(rows);
+};
 
 const createLikeByPaperId = async (client, userId, paperId) => {
-    const { rows } = await client.query(
-        `
+  const { rows } = await client.query(
+    `
         INSERT INTO "like"
         (user_id, paper_id)
         VALUES
         ($1, $2)
         RETURNING *
         `,
-        [userId, paperId],
-    );
-    return convertSnakeToCamel.keysToCamel(rows);
+    [userId, paperId],
+  );
+  return convertSnakeToCamel.keysToCamel(rows);
 };
 
 const updateLike = async (client, userId, paperId, isDeleted) => {
-    let newValue = true;
+  let newValue = true;
 
-    // isDeleted가 true인 경우 false로 바꾸기
-    if (isDeleted){
-        newValue = false;
-    }
-    
-    const { rows } = await client.query(
-      `
+  // isDeleted가 true인 경우 false로 바꾸기
+  if (isDeleted) {
+    newValue = false;
+  }
+
+  const { rows } = await client.query(
+    `
       UPDATE "like" l
       SET user_id = $1, paper_id = $2, is_deleted = $3
       WHERE paper_id = $2
       AND user_id = $1
       RETURNING * 
       `,
-      [userId, paperId, newValue],
-    );
-    return convertSnakeToCamel.keysToCamel(rows[0]);
-  };
+    [userId, paperId, newValue],
+  );
+  return convertSnakeToCamel.keysToCamel(rows[0]);
+};
 
-module.exports = {getLikeByPaperId, createLikeByPaperId, updateLike };
+module.exports = { getLikeByPaperId, createLikeByPaperId, updateLike };
--- functions/db/paper.js
+++ functions/db/paper.js
@@ -2,22 +2,22 @@ const _ = require('lodash');
 const convertSnakeToCamel = require('./../lib/convertSnakeToCamel');
 
 const getAllPaper = async (client) => {
-    const { rows } = await client.query(
-      `
+  const { rows } = await client.query(
+    `
       SELECT * FROM "paper"
       `,
-    );
+  );
   return convertSnakeToCamel.keysToCamel(rows);
 };
 
 const getPaperByPaperId = async (client, paperId) => {
-    const { rows } = await client.query(
-      `
+  const { rows } = await client.query(
+    `
       SELECT * FROM "paper"
       WHERE id = $1
       `,
-      [paperId],
-    );
+    [paperId],
+  );
   return convertSnakeToCamel.keysToCamel(rows);
 };
-module.exports = {getAllPaper, getPaperByPaperId };
+module.exports = { getAllPaper, getPaperByPaperId };
